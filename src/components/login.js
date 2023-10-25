f13b50be36ea71d94d58e56cf05d9161ff2d3e08
AmayGawas <amaygawas07@gmail.com>
2023-10-26 03:52:38 +0530

    cosmetic changes

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios';
 import {
+    
     Button,
     TextField,
     Grid,
@@ -15,9 +16,26 @@ import {
 
 import LockOpenIcon from '@mui/icons-material/LockOpen';
 import { useState } from 'react';
+import { makeStyles } from '@mui/styles';
+import backgroundImage from './resources/loginbg-01.png';
+
+const useStyles = makeStyles({
+  root: {
+    height: '100vh',
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  paper: {
+    padding: 20,
+  },
+});
 
 function Login() {
-
+  const classes = useStyles();
     const userData={id:"", password:""};
     const [inputData,setinputData]=useState(userData);
 
@@ -53,56 +71,60 @@ function Login() {
           }
         
     }
-  return (
-
-    <Box
-    sx={{
-      marginTop: 20,
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    }}
-  ><Typography variant='h4' sx={{mb:3}} >LOGIN</Typography>
-    <Paper elevation={3} sx={{p:4}}>
-    
-      <Box component="form"  sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              value={inputData.id}
-              label="Store_id/Employee_id"
-              autoFocus
-              onChange={handleData}
-              name="id"
-              type="text"
-              id="id"
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              value={inputData.password}
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              
-              onChange={handleData}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={submitData}
-            >
-              Sign In
-            </Button>
+    return (
+      <Grid container className={classes.root}>
+        <Grid item xs={12} sm={6} md={4}>
+          <Paper elevation={3} className={classes.paper}>
+            <Box
+              sx={{
+                marginTop: 20,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            ><Typography variant='h4' sx={{mb:3}} >Sales Billing</Typography>
+              <Paper elevation={3} sx={{p:4}}>
+                <Box component="form"  sx={{ mt: 1 }}>
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    value={inputData.id}
+                    label="Store_id/Employee_id"
+                    autoFocus
+                    onChange={handleData}
+                    name="id"
+                    type="text"
+                    id="id"
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    value={inputData.password}
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    
+                    onChange={handleData}
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    onClick={submitData}
+                  >
+                    Sign In
+                  </Button>
+                </Box>
+              </Paper>
             </Box>
-            </Paper>
-    </Box>
-    )
-}
+          </Paper>
+        </Grid>
+      </Grid>
+    );
+  }
 
 export default Login
